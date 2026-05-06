36df8023ac13cd71f593ac64ec17813b0acc81b2
Alex Villarejo i Garcia <mail@villarejoigarcia.com>
2026-05-07 00:25:05 +0200

    loader

--- src/app/components/loader.tsx
+++ src/app/components/loader.tsx
@@ -30,10 +30,27 @@ type ProjectsLoaderClientProps = {
   projects: ProjectItem[];
 };
 
+function shuffleArray<T>(arr: T[]): T[] {
+  const a = [...arr];
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
 export default function ProjectsGalleryClient({
   projects,
 }: ProjectsLoaderClientProps) {
   const [activeProjectIndex, setActiveProjectIndex] = useState(0);
+  const [leftProjects, setLeftProjects] = useState(projects);
+  const [rightProjects, setRightProjects] = useState(projects);
+
+  useEffect(() => {
+    setLeftProjects(shuffleArray(projects));
+    setRightProjects(shuffleArray(projects));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   // animation
 
@@ -42,7 +59,7 @@ export default function ProjectsGalleryClient({
 
     const intervalId = window.setInterval(() => {
       setActiveProjectIndex((currentIndex) => (currentIndex + 1) % projects.length);
-    }, 167);
+    }, 200);
 
     return () => window.clearInterval(intervalId);
   }, [projects.length]);
@@ -76,16 +93,14 @@ export default function ProjectsGalleryClient({
     <main
       className={`w-full`}
     >
-      <div className='py-[3px] px-[5px]'>
-      <p className='text-(--color-grey)!'>Fashion and still life photographer based in Barcelona and working worldwide.</p>
-      </div>
-        {projects.map((project, index) => {
+      <div>
+        {leftProjects.map((project, index) => {
           const isActive = index === activeProjectIndex;
 
           return (
             <div
               key={`${project.slug.current}-${index}`}
-              className={`absolute px-[3px] lg:top-[17.5px] top-[15.5px] left-0 ${isActive ? 'opacity-100' : 'opacity-0'}`}
+              className={`absolute left-0 ${isActive ? 'opacity-100' : 'opacity-0'}`}
             >
 
               {project.images?.[0] && (
@@ -93,11 +108,41 @@ export default function ProjectsGalleryClient({
                   key={project.images[0].asset._id}
                   src={urlFor(project.images[0]).url()}
                   alt={project.title}
-                  className="lg:w-auto w-screen h-auto lg:h-[66.667dvh] object-cover"
+                  className="lg:w-[50vw] w-screen h-[50dvh] lg:h-screen object-cover"
                 />
               )}
-              <div className='m-[3px]'>
-                <p>
+              <div className='m-[3px] absolute lg:top-1/3 top-1/2 -translate-y-1/2 mix-blend-overlay'>
+                <p className='text-white!'>
+                  {project.code}.{project.title}
+                </p>
+              </div>
+
+
+            </div>
+          );
+        })}
+      </div>  
+
+      <div>
+        {rightProjects.map((project, index) => {
+          const isActive = index === activeProjectIndex;
+
+          return (
+            <div
+              key={`${project.slug.current}-${index}`}
+              className={`absolute right-0 bottom-0 ${isActive ? 'opacity-100' : 'opacity-0'}`}
+            >
+
+              {project.images?.[0] && (
+                <img
+                  key={project.images[0].asset._id}
+                  src={urlFor(project.images[0]).url()}
+                  alt={project.title}
+                  className="lg:w-[50vw] w-screen h-[50dvh] lg:h-screen object-cover"
+                />
+              )}
+              <div className='m-[3px] absolute lg:top-1/3 top-1/2 -translate-y-1/2 mix-blend-overlay'>
+                <p className='text-white!'>
                   {project.code}.{project.title}
                 </p>
               </div>
@@ -105,23 +150,27 @@ export default function ProjectsGalleryClient({
           );
         })}
 
-      <div className='fixed bottom-0 left-0 w-full lg:px-[3px_8px] px-[3px_7px] flex items-end justify-between'>
+      </div>
+
+      <div className='fixed top-0 left-0 w-full px-[3px] flex justify-between mix-blend-overlay'>
         
-        <div className='lg:flex-1'>
-          <h1>Javi Dardo</h1>
+        <div className='lg:flex-[.5]'>
+          <h1 className='text-white!'>Javi Dardo</h1>
           {/* <Logo /> */}
         </div>
-      
-        <div className='lg:flex-1 lg:pb-[8px] pb-[7px] text-center'>
-          <h6>Barcelona, {time}</h6>
+
+
+        <div className='lg:flex-[.5] pt-[3px] flex' id='loader'>
+          <div className='flex flex-col items-start ml-auto mr-[3px]'>
+            <a className='text-white!' href="https://www.instagram.com/javidardo" target='_blank'>@javidardo</a>
+            <a className='text-white!' href="tel:+34 669 342 305">+34 669 342 305</a>
+            <a className='text-white!' href="mailto:contact@javidardo.com">contact@javidardo.com</a>
+          </div>
         </div>
 
-        <div className='lg:flex-1 lg:pb-[8px] pb-[7px] flex'>
-          <div className='flex flex-col items-start ml-auto'>
-            <a href="https://www.instagram.com/javidardo" target='_blank'>@javidardo</a>
-            <a href="tel:+34 669 342 305">+34 669 342 305</a>
-            <a href="mailto:contact@javidardo.com">contact@javidardo.com</a>
-          </div>
+        <div className='lg:flex-1 lg:py-[3px] py-[7px] text-right'>
+          {/* <h6>Barcelona, {time}</h6> */}
+          <p className='text-white! lg:relative lg:left-auto lg:top-auto fixed left-[3px] mt-[3px]! top-[50dvh]'>Fashion and still life photographer based in Barcelona and working worldwide</p>
         </div>
 
       </div>
